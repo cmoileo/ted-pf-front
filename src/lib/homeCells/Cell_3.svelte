282f755edf8ed8856793fcda1450819bebf8cff4
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-02-26 21:38:17 +0100

    style 3

--- src/lib/homeCells/Cell_3.svelte
+++ src/lib/homeCells/Cell_3.svelte
@@ -7,9 +7,33 @@
 </script>
 
 <div class="cell-3 main-radius">
-    {#each reseaux_sociaux as reseau_social}
-        <li class="relative">
-            <a class="abs-center" href={reseau_social.Lien} target="_blank"><img src={api_url+reseau_social.Image.data.attributes.url} alt=""></a>
-        </li>
-    {/each}
+    <li class="relative main-radius">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <img src="/Noise%20effect.png" class="noise abs-center w-full" alt="">
+        <a class="abs-center" href={reseaux_sociaux[0].Lien} target="_blank"><img src={api_url+reseaux_sociaux[0].Image.data.attributes.url} alt=""></a>
+    </li>
+    <li class="relativen main-radius">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <a class="abs-center" href={reseaux_sociaux[1].Lien} target="_blank"><img src={api_url+reseaux_sociaux[1].Image.data.attributes.url} alt=""></a>
+    </li>
+    <li class="relative main-radius">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <a class="abs-center" href={reseaux_sociaux[2].Lien} target="_blank"><img src={api_url+reseaux_sociaux[2].Image.data.attributes.url} alt=""></a>
+    </li>
+    <li class="relative main-radius">
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <a class="abs-center" href={reseaux_sociaux[3].Lien} target="_blank"><img src={api_url+reseaux_sociaux[3].Image.data.attributes.url} alt=""></a>
+    </li>
 </div>
