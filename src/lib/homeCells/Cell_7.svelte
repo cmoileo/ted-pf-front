29c59ca21bd6ee920e6e1703aeecbe5d1ccb0028
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-02-27 16:26:58 +0100

    style cell 7 + add swiper

--- src/lib/homeCells/Cell_7.svelte
+++ src/lib/homeCells/Cell_7.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
     import {type HomeDataType} from '$lib/types/homeData.type.ts'
     import {api_url} from "$lib/publicConsts.ts";
-
+    import { Swipe, SwipeItem } from "svelte-swipe";
+    const swipeConfig = {
+        autoplay: true,
+        autoplayInterval: 5000,
+        delay: 2000,
+        showIndicators: false,
+        transitionDuration: 500,
+        defaultIndex: 0,
+    };
     export let data: HomeDataType
     const images = data.Images_films.data
 </script>
 
 <div class="cell-7 main-radius relative">
-    <div class="images-c">
-        {#each images as image}
-            <img class="main-radius" src={api_url + image.attributes.url} alt=""/>
-        {/each}
+    <div class="images-c relative swipe-holder">
+        <Swipe {...swipeConfig}>
+            {#each images as image}
+                <SwipeItem>
+                    <img class="main-radius" src={api_url + image.attributes.url} alt=""/>
+                </SwipeItem>
+            {/each}
+        </Swipe>
     </div>
     <div class="txt-c txt-section-bottom">
         <p class="p-s grey-200">Passion</p>
