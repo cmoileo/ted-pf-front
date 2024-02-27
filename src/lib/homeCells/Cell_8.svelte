1568b5612194a4f1cd11d7507d783b040e2bfafd
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-02-27 18:48:27 +0100

    improve style cell 8

--- src/lib/homeCells/Cell_8.svelte
+++ src/lib/homeCells/Cell_8.svelte
@@ -8,7 +8,15 @@
     const lien_playlist = spotify.lien_playlist
 </script>
 
-<div class="cell-8 main-radius main-bg-gradiant">
+<div class="cell-8 main-radius main-bg-gradiant relative has-link-arrow">
+    <svg class="link-arrow" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 16.5L18 7.5C18 6.67157 17.3284 6 16.5 6L7.49997 6" stroke="#D1D1D1" stroke-width="2" stroke-linecap="round"/>
+        <path d="M17.5 6.5L6 18" stroke="#D1D1D1" stroke-width="2" stroke-linecap="round" stroke-linejoin="bevel"/>
+    </svg>
+    <span class="circle"></span>
+    <span class="circle"></span>
+    <span class="circle"></span>
+    <span class="circle"></span>
     <div class="txt-section-padding">
         <a target="_blank" href={lien_playlist} class="txt-c">
             <p class="p-s grey-200">Passion</p>
@@ -17,7 +25,7 @@
     </div>
     <div class="musiques-c">
         {#each musiques as musique}
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/{musique.id_musique}?utm_source=generator" allowtransparency="true" width="100%" height="50" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/{musique.id_musique}?utm_source=generator" allowtransparency="true" width="100%" height="60" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         {/each}
     </div>
 </div>
