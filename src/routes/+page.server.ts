92793003efa3d2b89de7f46e8187b770104e21bb
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-03-10 21:08:03 +0100

    update api url

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import {env} from "$env/dynamic/private";
 
 export async function load() {
-    const res = await fetch(`${env.API_URL}/api/home?populate=deep`)
+    const res = await fetch(`https://ted-pf-back.onrender.com/api/home?populate=deep`)
     const homeData = await res.json()
     return homeData.data.attributes
 }
